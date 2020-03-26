e28ac71c5e89293372c7abcaef6c676636c03d4f
Gabe Jensen <gabe.jensen@economicmodeling.com>
2020-03-25 20:24:22 -0700

    Add search box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to FastGif.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TextField id="searchbox" label="Search for a gif" variant="outlined" />
     </div>
   );
 }
