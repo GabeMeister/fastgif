27ad8db6eea953b739acc6ad671d371fa376d0cd
Gabe Jensen <gabe.jensen@economicmodeling.com>
2020-03-25 21:36:11 -0700

    Actually hit giphy and imgur apis

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import TextField from '@material-ui/core/TextField';
 
 function App() {
   const searchForGifs = text => {
-    console.log('searching for ' + text);
+    fetch(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${text}`, {
+      "method": "GET",
+      "headers": {}
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log('giphy: ', data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    fetch(`https://api.imgur.com/3/gallery/search/top/all?q=${text}&q_type=gif`, {
+      "method": "GET",
+      "headers": {
+        "authorization": `Client-ID ${process.env.REACT_APP_IMGUR_CLIENT_ID}`
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log('imgur: ', data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   return (
