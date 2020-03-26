bb404c016287a6b9b52a3faf8328bfa590ce947f
Gabe Jensen <gabe.jensen@economicmodeling.com>
2020-03-25 21:12:01 -0700

    add event handler

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 function App() {
+  const searchForGifs = text => {
+    console.log('searching for ' + text);
+  };
+
   return (
     <div>
-      <TextField id="searchbox" label="Search for a gif" variant="outlined" />
+      <TextField
+        id="searchbox"
+        label="Search for a gif"
+        variant="outlined"
+        onChange={evt => {
+          searchForGifs(evt.target.value);
+        }}
+      />
     </div>
   );
 }
